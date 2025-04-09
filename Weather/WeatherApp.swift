086bbaf6b43dc0db4aa33086c6d2532c6ba6c909
Luiz Furmann <furmannluiz@gmail.com>
2025-04-09 17:11:35 -0300

    initial commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Luiz Furmann on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
